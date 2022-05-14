98d30be822222142b39e8a0f9bdd659b66877ba4
Ákos Klobetz <klobetz@gmail.com>
2022-05-14 06:36:28 +0200

    #sima beolvasás osztályba

---
+++ Auto_erettsegi_sima_beolvasas/Adatszerkezet.cs
@@ -0,0 +1,12 @@
+﻿namespace Auto_erettsegi_sima_beolvasas
+{
+    internal class Adatszerkezet
+    {
+        public int Nap { get; set; }
+        public string Ido { get; set; }
+        public string Rendszam { get; set; }
+        public int Azonosito { get; set; }
+        public int Km { get; set; }
+        public int Kibehajtas { get; set; }
+    }
+}
---
+++ Auto_erettsegi_sima_beolvasas/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_erettsegi_sima_beolvasas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<Adatszerkezet>();
+
+            using (var fs = new FileStream("autok.txt", FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    //var fejlec = sr.ReadLine(); //a fejléc kivétele a fájlból
+                    while (!sr.EndOfStream)
+                    {
+                        var sor = sr.ReadLine().Split();
+                        var adat = new Adatszerkezet();
+
+                        adat.Nap = int.Parse(sor[0]);
+                        adat.Ido = sor[1];
+                        adat.Rendszam = sor[2];
+                        adat.Azonosito = int.Parse(sor[3]);
+                        adat.Km = int.Parse(sor[4]);
+                        adat.Kibehajtas = int.Parse(sor[5]);
+                        
+                        lista.Add(adat);
+                    }
+                }
+            }
+
+            foreach (var elem in lista)
+            {
+                Console.WriteLine(elem.Rendszam);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
