9674bb250a38bc5aea01c630e7c3953566224775
Ákos Klobetz <klobetz@gmail.com>
2022-06-10 12:43:02 +0200

    nevek konstruktoros beolvasása

---
+++ Nevek_beolvasasa_2keresztnev/Adatszerkezet.cs
@@ -0,0 +1,16 @@
+﻿namespace Nevek_beolvasasa_2keresztnev
+{
+    public class Adatszerkezet
+    {
+        public Adatszerkezet(string vnev, string knev1, string knev2)
+        {
+            this.vnev = vnev;
+            this.knev1 = knev1;
+            this.knev2 = knev2;
+        }
+
+        public string vnev { get; set; }
+        public string knev1 { get; set; }
+        public string knev2 { get; set; }
+    }
+}
---
+++ Nevek_beolvasasa_2keresztnev/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nevek_beolvasasa_2keresztnev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<Adatszerkezet>()                ;
+            using (var fs = new FileStream("Negyedeves.txt", FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    var fejlec = sr.ReadLine();
+                    while (!sr.EndOfStream)
+                    {
+                        var sor = sr.ReadLine().Split(' ');
+                        var adat = new Adatszerkezet(
+                            sor[0],
+                            sor[1],
+                            sor.Length > 2 ? sor[2] : ""      //egy soros iff-el lehet megcsinálni ha konstruktorba szeretnéd beolvasni
+                                                              //mert a 3. elemet is fel kell tölteni ilyenkor valamivel PL: "" (üres karakter)
+                        );
+
+                        lista.Add(adat);
+                    }
+                }
+            }
+
+            foreach (var item in lista)
+            {
+
+                Console.WriteLine($"{item.knev2}");
+
+
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+    
+}
