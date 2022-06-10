327960d42ab7e6c72bf26585a9a40c308c87373e
Ákos Klobetz <klobetz@gmail.com>
2022-06-10 13:18:35 +0200

    fibonacci

---
+++ Fibonacci_sorozat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_sorozat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            Console.WriteLine("Meddig írassam ki a sorozat elemeit: ");
+            var valasz = Console.ReadLine();
+
+            int szam;
+            while (!int.TryParse(valasz, out szam)||szam<0)
+            {
+                Console.WriteLine("Nem megfelelő az érték!");
+                valasz = Console.ReadLine();
+            }
+
+            for (int i = 0; i < int.Parse(valasz); i++)
+            {
+
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
