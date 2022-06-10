4d8aacf205e9f6ce2b874d5609a3f8af3f6792ee
Ákos Klobetz <klobetz@gmail.com>
2022-06-10 12:55:35 +0200

    Negyedéves beolvasas sima

---
+++ Negyedeves_sima/Adatszerkezet.cs
@@ -0,0 +1,11 @@
+﻿namespace Negyedeves_sima
+{
+    public class Adatszerkezet
+    {
+        public string megye { get; set; }
+        public int elson { get; set; }
+        public string masodikn { get; set; }
+        public string harmadikn { get; set; }
+        public string negyedikn { get; set; }
+    }
+}
---
+++ Negyedeves_sima/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negyedeves_sima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Adatszerkezet>();
+
+            using (var fs = new FileStream("Negyedeves.txt", FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    //var elsosor = sr.ReadLine(); ha az első sort ki kell hagyni!
+                    while (!sr.EndOfStream)
+                    {
+                        var a = new Adatszerkezet();
+                        a.megye = sr.ReadLine();
+                        a.elson = int.Parse(sr.ReadLine().Replace(" ", ""));
+                        a.masodikn = sr.ReadLine().Replace(" ", "");
+                        a.harmadikn = sr.ReadLine();
+                        a.negyedikn = sr.ReadLine();
+
+                        list.Add(a);
+
+                    }
+                }
+            }
+
+            Console.WriteLine($"a fájl {list.Count()} elemet tartalmaz\n");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.megye,-25}{item.elson}{item.masodikn,20}{item.harmadikn,20}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
