48fe760e79b7f4dd4fab272f13471d4cbab4f809
Ákos Klobetz <klobetz@gmail.com>
2022-06-10 12:56:17 +0200

    negyedéves konstruktorba

---
+++ Negyedeves_konstruktoros/Adatszerkezet.cs
@@ -0,0 +1,20 @@
+﻿namespace Negyedeves_konstruktoros
+{
+    public class Adatszerkezet
+    {
+        public Adatszerkezet(string megye, int elson, int masodikn, int harmadikn, int negyedikn)
+        {
+            this.megye = megye;
+            this.elson = elson;
+            this.masodikn = masodikn;
+            this.harmadikn = harmadikn;
+            this.negyedikn = negyedikn;
+        }
+
+        public string megye { get; set; }
+        public int elson { get; set; }
+        public int masodikn { get; set; }
+        public int harmadikn { get; set; }
+        public int negyedikn { get; set; }
+    }
+}
---
+++ Negyedeves_konstruktoros/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negyedeves_konstruktoros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Adatszerkezet>();
+
+            using (var fs = new FileStream("Negyedeves.txt", FileMode.Open))
+            {
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    //var elsosor = sr.ReadLine(); ha az első sort ki kell hagyni!
+                    while (!sr.EndOfStream)
+                    {
+                        var a = new Adatszerkezet(
+                        sr.ReadLine(),
+                        int.Parse(sr.ReadLine().Replace(" ", "")),
+                        int.Parse(sr.ReadLine().Replace(" ", "")),
+                        int.Parse(sr.ReadLine().Replace(" ", "")),
+                        int.Parse(sr.ReadLine().Replace(" ", ""))
+                        );
+                        list.Add(a);
+
+                    }
+                }
+            }
+
+            Console.WriteLine($"a fájl {list.Count()} elemet tartalmaz\n");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.megye,-25}{item.elson}{item.masodikn,20}{item.harmadikn,20}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
